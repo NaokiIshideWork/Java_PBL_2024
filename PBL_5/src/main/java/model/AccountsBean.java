9684016a0236adbe329ff3b65c108f3a6c99c28f
Sota Ogura <User@WINDOWS-GQ0ELMF>
2024-06-10 16:31:07 +0900

    aaa

--- PBL_5/src/main/java/model/AccountsBean.java
+++ PBL_5/src/main/java/model/AccountsBean.java
@@ -2,4 +2,21 @@ package model;
 
 public class AccountsBean {
 
+	private int account_id;
+	private String test;
+	private String mail;
+	private String password;
+	private int authority;
+	
+	public AccountsBean(int ai, String t, String m, String p, int au) {
+		account_id = ai;
+		test = t;
+		mail = m;
+		password = p;
+		authority = au;
+	}
+	
+	
+	
+	
 }
