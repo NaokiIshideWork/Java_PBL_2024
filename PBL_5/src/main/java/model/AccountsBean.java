6010024d69dba4431e580f0df62768794ac24d5d
Sota Ogura <User@WINDOWS-GQ0ELMF>
2024-06-10 16:53:51 +0900

    acountsBean追加

--- PBL_5/src/main/java/model/AccountsBean.java
+++ PBL_5/src/main/java/model/AccountsBean.java
@@ -7,7 +7,7 @@ public class AccountsBean {
 	private String mail;
 	private String password;
 	private int authority;
-	
+
 	public AccountsBean(int ai, String t, String m, String p, int au) {
 		account_id = ai;
 		test = t;
@@ -15,8 +15,26 @@ public class AccountsBean {
 		password = p;
 		authority = au;
 	}
+
+	public int getAccount_id() {
+		return account_id;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int getAuthority() {
+		return authority;
+	}
 	
-	
-	
-	
+
 }
