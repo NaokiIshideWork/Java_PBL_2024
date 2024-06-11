2460805da5bbdf8356ae0f0a9f72f91dcbbecec5
User <User@WINDOWS-STGNLPH>
2024-06-11 13:12:29 +0900

    bean追加

--- PBL_5/src/main/java/model/SalesBean.java
+++ PBL_5/src/main/java/model/SalesBean.java
@@ -1,42 +1,38 @@
 package model;
 
 public class SalesBean {
-	
-	private int sale_id;
+
 	private String sale_date;
-	private int account_id;
-	private int category_id;
+	private String account;
+	private String item_category;
 	private String trade_name;
 	private int unit_price;
 	private int sale_number;
+	private int subtotal;
 	private String note;
-	
-	public SalesBean(int si,String sd,int ai,int ci,String tn,int up,int sn,String n) {
-		sale_id=si;
-		sale_date=sd;
-		account_id=ai;
-		category_id=ci;
-		trade_name=tn;
-		unit_price=up;
-		sale_number=sn;
-		note=n;
+
+	public SalesBean( String sd, String a,String ic, String tn, int up, int sn,int st,  String n) {
 		
+		sale_date = sd;
+		account=a;
+		item_category=ic;
+		trade_name = tn;
+		unit_price = up;
+		sale_number = sn;
+		subtotal=st;
+		note = n;
+
 	}
 
-	public int getSale_id() {
-		return sale_id;
-	}
 
 	public String getSale_date() {
 		return sale_date;
 	}
-
-	public int getAccount_id() {
-		return account_id;
+	public String getAccout_id() {
+		return account;
 	}
-
-	public int getCategory_id() {
-		return category_id;
+	public String getItem_category() {
+		return item_category;
 	}
 
 	public String getTrade_name() {
@@ -50,11 +46,13 @@ public class SalesBean {
 	public int getSale_number() {
 		return sale_number;
 	}
+	
+	public int getSubtotal() {
+		return subtotal;
+	}
 
 	public String getNote() {
 		return note;
 	}
-	
-	
 
 }
