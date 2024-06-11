5353cbabc84ae507419493c6f23d1688d19020aa
User <User@WINDOWS-SABG9PG>
2024-06-11 17:24:52 +0900

    追加

--- PBL_5/src/main/java/controller/RegisterSalesServlet.java
+++ PBL_5/src/main/java/controller/RegisterSalesServlet.java
@@ -34,7 +34,6 @@ public class RegisterSalesServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		this.getServletContext().getRequestDispatcher("/S0011.jsp").forward(request, response);
 	}
-	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -46,28 +45,28 @@ public class RegisterSalesServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		SQLServicesPBLreg mts = new SQLServicesPBLreg();
 		SalesBean list = null;
-		
+
 		String sales_date = request.getParameter("sale_date");
-		
+
 		//担当に対するName
 		String account_id = request.getParameter("account");//id	
 		String accountName = mts.SelectRegName(Integer.parseInt(account_id));
-		
+
 		//商品カテゴリーに対する
 		String item_category_id = request.getParameter("item_category");//id
 		String item_category_name = mts.SelectCategory_Name(Integer.parseInt(item_category_id));
-		
+
 		//商品名以下
 		String trade_name = request.getParameter("trade_name");
 		String unit_price = request.getParameter("unit_price");
 		String sale_number = request.getParameter("sale_number");
-		String note = request.getParameter("note");	
-		int subtotal = Integer.parseInt(unit_price)*Integer.parseInt(sale_number);
-		
-		
-		list = new SalesBean(sales_date, accountName ,Integer.parseInt(account_id), item_category_name,Integer.parseInt(item_category_id),trade_name,Integer.parseInt(unit_price),
+		String note = request.getParameter("note");
+		int subtotal = Integer.parseInt(unit_price) * Integer.parseInt(sale_number);
+
+		list = new SalesBean(sales_date, accountName, Integer.parseInt(account_id), item_category_name,
+				Integer.parseInt(item_category_id), trade_name, Integer.parseInt(unit_price),
 				Integer.parseInt(sale_number), subtotal, note);
-		
+
 		request.setAttribute("list", list);
 		doGet(request, response);
 	}
