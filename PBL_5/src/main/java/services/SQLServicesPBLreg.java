a4f3b26f74008d9c527bce7d572c7a2db14eae3d
User <User@WINDOWS-STGNLPH>
2024-06-11 15:04:34 +0900

    追加

--- PBL_5/src/main/java/services/SQLServicesPBLreg.java
+++ PBL_5/src/main/java/services/SQLServicesPBLreg.java
@@ -7,10 +7,32 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.AccountsBean;
+import model.CategoriesBean;
 import util.DbUtil;
 
 public class SQLServicesPBLreg {
-//	ああ」
+
+	public ArrayList<CategoriesBean> SelectAllCategory() {
+		String sql = "SELECT * FROM categories WHERE active_flg = 1;";
+		ArrayList<CategoriesBean> categories_list = new ArrayList<CategoriesBean>();
+		try (
+				Connection con = DbUtil.open();
+				PreparedStatement ps = con.prepareStatement(sql);) {
+			// PreparedStatementがクローズされるタイミングでクローズされる
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				int category_id = rs.getInt("category_id");
+				String category_name = rs.getString("category_name");
+				int active_flg = rs.getInt("active_flg");
+
+				categories_list.add(new CategoriesBean(category_id, category_name, active_flg));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return categories_list;
+	}
+
 	public ArrayList<AccountsBean> SelectAllAcount() {
 		String sql = "SELECT * FROM accounts WHERE authority = 1 OR authority =11 ; ";
 		ArrayList<AccountsBean> account_list = new ArrayList<AccountsBean>();
@@ -26,13 +48,12 @@ public class SQLServicesPBLreg {
 				String password = rs.getString("password");
 				int authority = rs.getInt("authority");
 
-				
 				account_list.add(new AccountsBean(account_id, name, mail,
 						password, authority));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return account_list ;
+		return account_list;
 	}
 }
