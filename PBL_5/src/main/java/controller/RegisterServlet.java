a189bad997b97595da95bd4690e254253f1eff44
User <User@WINDOWS-SABG9PG>
2024-06-12 11:17:32 +0900

    追加

--- PBL_5/src/main/java/controller/RegisterServlet.java
+++ PBL_5/src/main/java/controller/RegisterServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.AccountsBean;
 import model.CategoriesBean;
+import model.SalesBean;
 import services.SQLServicesPBLreg;
 /**
  * Servlet implementation class RegisterServlet
@@ -55,7 +56,7 @@ public class RegisterServlet extends HttpServlet {
 		}else {
 			request.setAttribute("cate", categories_list);
 		}
-				
+		System.out.println(error_message);	
 		this.getServletContext().getRequestDispatcher("/S0010.jsp").forward(request, response);
 	}
 
@@ -64,28 +65,106 @@ public class RegisterServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+
 		request.setCharacterEncoding("UTF-8");
 		SQLServicesPBLreg mts = new SQLServicesPBLreg();
-		
-		
+		SalesBean list = null;
+		String error_message = "";
+
 		String sales_date = request.getParameter("sale_date");
-		
-		//account_id
-		String account_id =  request.getParameter("account_id");
-		
-		//categori_id
-		String category_id = request.getParameter("category_id");
-			
+		if (sales_date.isEmpty()) {
+			error_message += "販売日を入力して下さい,";//ok
+		} else {
+			//販売日を正しく入力して下さい
+		}
+
+		//担当に対するName
+		String account_id = request.getParameter("account");//id	
+		String accountName = mts.SelectRegName(Integer.parseInt(account_id));//担当名
+		if (accountName.isEmpty()) {
+			error_message += "担当が未選択です,";//ok
+		}
+
+		//商品カテゴリーに対する
+		String item_category_id = request.getParameter("item_category");//id
+		String item_category_name = mts.SelectCategory_Name(Integer.parseInt(item_category_id));
+		if (item_category_name.isEmpty()) {
+			error_message += "商品カテゴリーが未選択です,";//ok
+		}
+
+		//商品名
 		String trade_name = request.getParameter("trade_name");
+		if (trade_name.isEmpty()) {
+			error_message += "商品名を入力して下さい,";//ok
+		} else {
+			if (trade_name.length() > 100) {
+				error_message += "商品名が長すぎます,";//ok
+			}
+		}
+		
+
+		//単価
 		String unit_price = request.getParameter("unit_price");
+		if (unit_price.isEmpty()) {
+			error_message += "単価を入力して下さい,";
+		} else {
+			if (unit_price.length() > 10) {
+				error_message += "単価が長すぎます,";
+			} else {
+				//形式チェック　単価を正しく入力して下さい
+				if (!unit_price.matches("\\d+?(,\\d{3})*")) {
+					error_message += "単価を正しく入力して下さい,";
+				}
+			}
+		}
+		System.out.println(error_message);
+		System.out.println(unit_price);
+		
+		
+		
+		//個数
 		String sale_number = request.getParameter("sale_number");
+		if (sale_number.isEmpty()) {
+			error_message += "個数を入力して下さい,";
+		} else {
+			if (sale_number.length() > 10) {
+				error_message += "個数が長すぎます,";
+			} else {
+				//個数を正しく入力して下さい
+				if (!sale_number.matches("\\d+?(,\\d{3})*")) {
+					error_message += "個数を正しく入力して下さい,";
+				}
+			}
+		}
+
+		//備考
 		String note = request.getParameter("note");
+		if (note.length() > 400) {
+			error_message += "備考が長すぎます,";
+		}
+
+		int subtotal = 0;
+		if (!(unit_price.isEmpty()) && !(sale_number.isEmpty())) {
+			subtotal = Integer.parseInt(unit_price) * Integer.parseInt(sale_number);
+		}
+		System.out.println(error_message);
+		System.out.println(error_message.isEmpty());
+
+		if (error_message.isEmpty()) {
+			list = new SalesBean(sales_date, accountName, Integer.parseInt(account_id), item_category_name,
+					Integer.parseInt(item_category_id), trade_name, Integer.parseInt(unit_price),
+					Integer.parseInt(sale_number), subtotal, note);
+			request.setAttribute("list", list);
+			this.getServletContext().getRequestDispatcher("/S0011.jsp").forward(request, response);
+		} else {
+			request.setAttribute("err", error_message);
+			doGet(request, response);
+		}
+
+	}
 		
-		System.out.println(sales_date+account_id+category_id);
-		System.out.println(trade_name+unit_price+sale_number+note);
-		
-		mts.insert(sales_date, Integer.parseInt(account_id), Integer.parseInt(category_id), trade_name, Integer.parseInt(unit_price), Integer.parseInt(sale_number), note);
-		doGet(request, response);
 	}
 
-}
+
