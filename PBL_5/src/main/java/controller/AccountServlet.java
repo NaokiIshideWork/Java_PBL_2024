c5d81bbd62d63989bbb0be6314e1423d6f846eb8
Sota Ogura <User@WINDOWS-GQ0ELMF>
2024-06-13 15:06:32 +0900

    追加

--- PBL_5/src/main/java/controller/AccountServlet.java
+++ PBL_5/src/main/java/controller/AccountServlet.java
@@ -35,8 +35,6 @@ public class AccountServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
-        
         response.setContentType("text/html: charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		
