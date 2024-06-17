d284d0bf6e7054f3b763eb108fc9a3d653778d96
NaokiIshideWork <naoki_ishide@trustsystem.co.jp>
2024-06-17 11:47:10 +0900

    追加

--- PBL_5/src/main/java/services/SQLservicesPBL.java
+++ PBL_5/src/main/java/services/SQLservicesPBL.java
@@ -4,10 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import model.AccountsBean;
-import model.CategoriesBean;
 import util.DbUtil;
 
 public class SQLservicesPBL {
@@ -45,53 +43,6 @@ public class SQLservicesPBL {
 		return person;
 	}
 
-	public ArrayList<CategoriesBean> SelectAllCategory() {
-		String sql = "SELECT * FROM categories WHERE active_flg = 1;";
-		CategoriesBean categories_list = null;
-		try (
-				Connection con = DbUtil.open();
-				PreparedStatement ps = con.prepareStatement(sql);) {
-			// PreparedStatementがクローズされるタイミングでクローズされる
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				int category_id = rs.getInt("category_id");
-				String category_name = rs.getString("category_name");
-				int active_flg = rs.getInt("active_flg");
-
-				categories_list = new CategoriesBean(category_id, category_name, active_flg);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return categories_list;
-	}
-
-	public ArrayList<AccountsBean> SelectAllAcount() {
-
-		String sql = "SELECT * FROM accounts WHERE authority = 1 OR authority =11 ; ";
-		ArrayList<AccountsBean> account_list = new ArrayList<AccountsBean>();
-		try (
-				Connection con = DbUtil.open();
-				PreparedStatement ps = con.prepareStatement(sql);) {
-			// PreparedStatementがクローズされるタイミングでクローズされる
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				int account_id = rs.getInt("account_id");
-				String name = rs.getString("name");
-				String mail = rs.getString("mail");
-				String password = rs.getString("password");
-				int authority = rs.getInt("authority");
-
-				account_list = new AccountsBean(account_id, name, mail, password, authority);
-
-				account_list.add(new AccountsBean(account_id, name, mail,
-						password, authority));
-
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return account_list;
-	}
+	
 
 }
